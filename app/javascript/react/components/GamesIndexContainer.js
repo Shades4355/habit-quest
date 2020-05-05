f26973207b745c66417240275dbd93b0909c3a64
Shades <mydude@Shadess-MBP.home>
2020-05-05 08:37:03 -0400

    index page started

---
+++ app/javascript/react/components/GamesIndexContainer.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+const GamesIndexContainer = props => {
+
+  useEffect = () =>{
+    fetch()
+  }
+/api/v1/habits
+
+  return(
+    "Hello from index"
+  )
+}
+
+export default GamesIndexContainer
