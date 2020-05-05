fa844fc3b932b651144c826200799bd04b786d12
Shades <mydude@Shadess-MBP.home>
2020-05-05 08:37:03 -0400

    index page started

---
+++ app/javascript/react/components/GamesIndexContainer.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+
+import HabitTile from './HabitTile'
+
+const GamesIndexContainer = props => {
+  const [getHabits, setHabits] = useState([{}])
+
+  useEffect(() =>{
+    fetch('/api/v1/habits')
+    .then(response => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.statuse} (${response.statusText})`,
+        error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json)
+    .then(habits => {
+      setHabits(habits)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  },[])
+
+  debugger
+
+  let habitTile = getHabits.map((habit) =>{
+    return (
+      <HabitTile
+        key={habit.id}
+        id={habit.id}
+        name={habit.name}
+      />
+    )
+  })
+
+  if (getHabits === [{}]) {
+    return(
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+       {habitTile}
+      </div>
+    )
+  }
+}
+
+export default GamesIndexContainer
---
+++ app/javascript/react/components/HabitTile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const HabitTile = props => {
+  debugger
+  return(
+    <div>
+      hi from tile
+    </div>
+  )
+}
+
+export default HabitTile
